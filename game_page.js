f6c7116a30e4575ec6fffcebaf4948e0b37b9353
sanvisubhi123 <76211547+sanvisubhi123@users.noreply.github.com>
2021-06-27 11:56:15 +0530

    Add files via upload

--- game_page.js
+++ game_page.js
@@ -6,8 +6,8 @@ document.getElementById("player1_name").innerHTML = player1_name + ":";
 document.getElementById("player2_name").innerHTML = player2_name + ":";
 document.getElementById("player1_score").innerHTML = player1_score;
 document.getElementById("player2_score").innerHTML = player2_score;
-document.getElementById(player_question).innerHTML = "question Turn - " + player1_name;
-document.getElementById(player_answer).innerHTML = "answer Turn - " + player2_name;
+document.getElementById("player_question").innerHTML = "Question Turn - " + player1_name ;
+document.getElementById("player_answer").innerHTML = "Answer Turn - " + player2_name ;
 
 function send() {
     get_word = document.getElementById("word").value;
